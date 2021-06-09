476ffa5d75b15221ccf8499b624a79305019cbbf
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-06-09 15:25:57 +0000

    delta calc system changes consistency, test

--- vasp_interactive/vasp_interactive.py
+++ vasp_interactive/vasp_interactive.py
@@ -270,6 +270,11 @@ class VaspInteractive(Vasp):
     ):
         check_atoms(atoms)
 
+        # check if system changes have been put here by delta calc
+        if hasattr(self, "system_changes") and self.system_changes is not None:
+            system_changes = self.system_changes
+            self.system_changes = None
+
         if not system_changes:
             # No need to calculate, calculator has stored calculated results
             return
