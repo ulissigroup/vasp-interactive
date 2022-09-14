3da55b41f8f64dfa879289a8b1abbea0d42386af
T.Tian <alchem0x2a@gmail.com>
2022-09-14 14:08:16 -0700

    add cori job wrappers

---
+++ tests/job_test_cori_hsw.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -N 1
+#SBATCH -C haswell
+#SBATCH -q premium
+#SBATCH -A m2755
+#SBATCH -t 06:00:00
+
+root="/global/u1/t/ttian20/vasp-interactive-test"
+export VASP_COMMAND="srun -n 32 -c 2 --cpu-bind=cores vasp_std"
+# vpi is the conda environment prebuilt
+conda activate vpi
+pip install -e $root
+for ver in "vasp/5.4.4-hsw" "vasp/6.3.0-hsw"
+do
+    module load $ver
+    echo "Testing VaspInteractive on $ver"
+    for f in $root/tests/test*.py
+    do
+        pytest -svv $f; killall vasp_std || echo ""
+    done
+    module unload vasp
+done
