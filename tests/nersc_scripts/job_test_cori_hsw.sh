e2b06c6b2ae6deefdd8acc38e5663e2111344075
T.Tian <alchem0x2a@gmail.com>
2022-09-21 08:43:57 -0700

    move job scripts

---
+++ tests/nersc_scripts/job_test_cori_hsw.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -l
+#SBATCH -N 1
+#SBATCH -C haswell
+#SBATCH -q premium
+#SBATCH -A m2755
+#SBATCH -t 06:00:00
+
+export VASP_COMMAND="srun -n 32 -c 2 --cpu-bind=cores vasp_std"
+GIT_REPO="ulissigroup/vasp-interactive"
+if [ -z "$GIT_REF" ]
+then
+    GIT_REF="main"
+fi
+conda activate vpi
+
+uid=`uuidgen`
+root=$SCRATCH/vpi-runner/$uid
+mkdir -p $root && cd $root
+jobid=${SLURM_JOB_ID}
+echo "Job ID $jobid"
+echo "Running tests under $root"
+gh repo clone $GIT_REPO
+cd vasp-interactive
+git checkout $GIT_REF
+echo "Check to $GIT_REF"
+export PYTHONPATH=`realpath .`
+
+res="true"
+for ver in "vasp/5.4.4-hsw" "vasp/6.3.0-hsw"
+do
+    module load $ver
+    echo "Testing VaspInteractive on $ver"
+    for f in tests/test*.py
+    do
+        pytest -svv $f; killall vasp_std || echo ""
+        if [[ $? != 0 ]]
+        then
+            res="false"
+            break
+        fi
+    done
+    module unload vasp
+    if [[ $res == "false" ]]
+    then
+        break
+    fi
+done
+
+if [[ $res == "true" ]]
+then
+    echo "All test pass!"
+else
+    echo "Test fail. See output"
+fi
+
+
+gh workflow run cori_hsw_status.yaml -f signal=$res -f jobid=$jobid -f path=$root
--- tests/nersc_scripts/job_test_perlmutter_cpu.sh
+++ tests/nersc_scripts/job_test_perlmutter_cpu.sh
@@ -1,16 +1,20 @@
 #!/bin/bash -l
 #SBATCH -N 1
-#SBATCH -C haswell
+#SBATCH -C cpu
 #SBATCH -q premium
 #SBATCH -A m2755
 #SBATCH -t 06:00:00
 
+
+
 root="/global/u1/t/ttian20/vasp-interactive-test"
-export VASP_COMMAND="srun -n 32 -c 2 --cpu-bind=cores vasp_std"
+export OMP_NUM_THREADS=1
+export VASP_COMMAND="srun -n 8 -c 32 --cpu-bind=cores vasp_std"
 # vpi is the conda environment prebuilt
+module load python
 conda activate vpi
 pip install -e $root
-for ver in "vasp/5.4.4-hsw" "vasp/6.3.0-hsw"
+for ver in  "vasp/5.4.4-cpu" "vasp/6.3.2-cpu"
 do
     module load $ver
     echo "Testing VaspInteractive on $ver"
