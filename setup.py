b175810cd7c742224cdb5791b0e02ce0f939e916
T.Tian <alchem0x2a@gmail.com>
2022-09-23 22:59:36 -0400

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name="vasp-interactive",
-    version="0.0.9",
+    version="0.0.10",
     packages=["vasp_interactive", "vasp_interactive.kubernetes"],
     install_requires=[
         "ase",
