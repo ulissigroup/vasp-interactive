90803a804480694dad269e3d920d65cb9fbb5e7c
T.Tian <alchem0x2a@gmail.com>
2022-10-07 02:01:38 +0000

    update patches file

--- patches/patch.py
+++ patches/patch.py
@@ -10,20 +10,20 @@ fpath = Path(__file__)
 fdir = fpath.parent
 
 
-def patch_txt(old_file, pattern, patch_file):
-    """Backup the old file and write the patch
-    """
+def patch_txt(old_file, pattern, patch_file, replace_func):
+    """Backup the old file and write the patch"""
     old_file, patch_file = Path(old_file), Path(patch_file)
     txt = open(old_file, "r").read()
-    repl = open(patch_file, "r").read()
-    re.sub(pattern, repl, txt, count=1, flags=(re.MULTILINE | re.IGNORECASE))
+    repl = replace_func(open(patch_file, "r").read())
+    matches = re.findall(pattern, txt, flags=(re.MULTILINE | re.IGNORECASE))
+    assert len(matches) == 1
+    # print(matches[0])
+    txt_new = re.sub(pattern, repl, txt, count=1, flags=(re.MULTILINE | re.IGNORECASE))
     backup_file = old_file.with_suffix(old_file.suffix + ".bak")
     shutil.copy(old_file, backup_file)
     with open(old_file, "w") as fd:
-        fd.write(txt)
+        fd.write(txt_new)
     return
-    
-    
 
 
 patches = []
@@ -44,6 +44,7 @@ patches.append(
             r"[\s\S]*?ENDIF[\s\S]*?\n"
         ),
         "file": "main.F.patch",
+        "replace_func": lambda s: s,
     }
 )
 patches.append(
@@ -57,27 +58,33 @@ patches.append(
                     !*************************SUBROUTINE OUTPOS_TRAIL  *********************
                     """,
         "pattern": (
-            r"^\s*?END\sSUBROUTINE\sINPOS.*?\n{2}"
-            r"([\s\S]*?)"
-            r"\n.*?SUBROUTINE\sOUTPOS_TRAIL"
+            r"(^\s*?END\sSUBROUTINE\sINPOS.*?$\n{2})"
+            r"([\s\S\n]*?)"
+            r"(^\!\*+?SUBROUTINE\sOUTPOS_TRAIL)"
         ),
         "file": "poscar.F.patch",
+        "replace_func": lambda s: r"\1" + s + r"\3",
     }
 )
 
+
 def main():
-    parser = ArgumentParser(description='Patch VASP Fortran files for better interactive mode integration')
-    parser.add_argument('src', type=str, help='Path to VASP source code')
+    parser = ArgumentParser(
+        description="Patch VASP Fortran files for better interactive mode integration"
+    )
+    parser.add_argument("src", type=str, help="Path to VASP source code")
     args = parser.parse_args()
     src = Path(os.path.expanduser(args.src)).resolve()
     for patch in patches:
         old_file = src / patch["name"]
         patch_file = fdir / patch["file"]
         pattern = patch["pattern"]
+        func = patch["replace_func"]
         print(f"Making patch for {old_file.as_posix()}")
-        patch_txt(old_file, pattern, patch_file)
+        patch_txt(old_file, pattern, patch_file, func)
     print("Success")
     return
+
 
 if __name__ == "__main__":
     main()
