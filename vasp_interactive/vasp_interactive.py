561645a5dbfdf56e03ce007ca5c288276cb6f63b
T.Tian <alchem0x2a@gmail.com>
2022-10-11 20:37:26 +0000

    add mandatory isif=3 to input params

--- vasp_interactive/vasp_interactive.py
+++ vasp_interactive/vasp_interactive.py
@@ -48,6 +48,8 @@ class VaspInteractive(Vasp):
         "interactive": True,
         # Disable stopping criteria but just rely on nsw
         "ediffg": 0,
+        # Set ISIF tag to cature stress-induced energy change
+        "isif": 3,
     }
     # Enforce the job to run infinitely untill killed
     default_input = {
